e0cfcf1b74078affbcfad5b66abb93fb1f62b5c5
San4es <mister.San4es@list.ru>
2021-08-01 16:15:13 +0400

    added insights

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,15 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 2000
   })
+
+  $(".star").rateYo({
+    rating: 3.6,
+    // data-rateyo-rating="3.6", 
+    starWidth: "40px",
+    normalFill: "#A0A0A0",
+    ratedFill: "#E74C3C",
+    readOnly: true
+  });
+
+
 });
